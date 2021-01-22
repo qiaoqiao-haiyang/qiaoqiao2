81b99643cbffd18889c1c169bbe2da955ef680c1
乔鑫 <59754675+qiaoqiao-haiyang@users.noreply.github.com>
2021-01-22 11:03:45 +0800

    webpack插件和服务与热更新

--- mywebpack5/webpack.config.js
+++ mywebpack5/webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -12,6 +13,22 @@ module.exports = {
         filename:'[name].js'
     },
     module:{},
-    plugins:[],
-    devServer:{}
+    //插件
+    plugins:[
+        new HtmlWebpackPlugin({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            template:'./src/index.html'
+        })
+    ],
+    //服务与热更新
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'127.0.0.1',
+        compress:true,
+        port:8081,
+        
+    }
 }
