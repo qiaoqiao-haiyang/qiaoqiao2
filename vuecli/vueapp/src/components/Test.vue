3a36123a05ee550e699739536832ccc827453d7e
乔鑫 <59754675+qiaoqiao-haiyang@users.noreply.github.com>
2021-01-27 11:40:45 +0800

    router的配置

---
+++ vuecli/vueapp/src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <button @click="addHandle">按钮</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            num:123
+        }
+    },
+    props:['msg'],
+    methods: {
+        addHandle(){
+            ++this.num
+        }
+    },
+}
+</script>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
---
+++ vuecli/vueapp/src/views/Page1.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>这里是页面1</h1>
+    </div>
+</template>
